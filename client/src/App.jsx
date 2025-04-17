6694056ea20b7a4f7a4b83c9041dbad2c3fdf6de
sovanghorai <sovanghorai6@gmail.com>
2025-04-17 10:44:33 +0530

    initilize g64 in main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,76 +1,77 @@
-import React, { useEffect } from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import { Toaster } from "react-hot-toast"
-import { useAppContext } from './context/AppContext'
-import Login from './components/Login'
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
 
 // Pages
-import Home from './pages/Home'
-import AllProducts from './pages/AllProducts'
-import ProductCategory from './pages/ProductCategory'
-import ProductDetails from './pages/ProductDetails'
-import Cart from './pages/Cart'
-import AddAddress from './pages/AddAddress'
-import MyOrders from './pages/MyOrders'
-import Loading from './components/Loading'
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import AddAddress from "./pages/AddAddress";
+import MyOrders from "./pages/MyOrders";
+import Loading from "./components/Loading";
 
 // Seller Pages
-import SellerLogin from './components/seller/SellerLogin'
-import SellerLayout from './pages/seller/SellerLayout'
-import AddProduct from './pages/seller/AddProduct'
-import ProductList from './pages/seller/ProductList'
-import Orders from './pages/seller/Orders'
+import SellerLogin from "./components/seller/SellerLogin";
+import SellerLayout from "./pages/seller/SellerLayout";
+import AddProduct from "./pages/seller/AddProduct";
+import ProductList from "./pages/seller/ProductList";
+import Orders from "./pages/seller/Orders";
 
 // Google Analytics
-import ReactGA from 'react-ga4'
-
-// Replace this with your actual GA4 Measurement ID
-const GA_MEASUREMENT_ID = 'G-Q987JF67KQ'
+import ReactGA from "react-ga4";
 
 const App = () => {
-  const location = useLocation()
-  const isSellerPath = location.pathname.includes("seller")
-  const { showUserLogin, isSeller } = useAppContext()
-
-  // Initialize GA only once
-  useEffect(() => {
-    ReactGA.initialize(GA_MEASUREMENT_ID)
-  }, [])
+  const location = useLocation();
+  const isSellerPath = location.pathname.includes("seller");
+  const { showUserLogin, isSeller } = useAppContext();
 
   // Track pageviews on route change
   useEffect(() => {
-    ReactGA.send({ hitType: 'pageview', page: location.pathname })
-  }, [location])
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname,
+      title: "Home Page",
+    });
+  }, [location]);
 
   return (
-    <div className='text-default min-h-screen text-gray-700 bg-white'>
+    <div className="text-default min-h-screen text-gray-700 bg-white">
       {!isSellerPath && <Navbar />}
       {showUserLogin && <Login />}
       <Toaster />
 
-      <div className={`${isSellerPath ? '' : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}>
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/products' element={<AllProducts />} />
-          <Route path='/products/:category' element={<ProductCategory />} />
-          <Route path='/products/:category/:id' element={<ProductDetails />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/add-address' element={<AddAddress />} />
-          <Route path='/my-orders' element={<MyOrders />} />
-          <Route path='/loader' element={<Loading />} />
-          <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="/products/:category/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/add-address" element={<AddAddress />} />
+          <Route path="/my-orders" element={<MyOrders />} />
+          <Route path="/loader" element={<Loading />} />
+          <Route
+            path="/seller"
+            element={isSeller ? <SellerLayout /> : <SellerLogin />}
+          >
             <Route index element={isSeller ? <AddProduct /> : null} />
-            <Route path='product-list' element={<ProductList />} />
-            <Route path='orders' element={<Orders />} />
+            <Route path="product-list" element={<ProductList />} />
+            <Route path="orders" element={<Orders />} />
           </Route>
         </Routes>
       </div>
 
       {!isSellerPath && <Footer />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,11 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
+import ReactGA from 'react-ga4'
+
+// Replace with your actual GA4 Measurement ID
+const GA_MEASUREMENT_ID = 'G-Q987JF67KQ'
+ReactGA.initialize(GA_MEASUREMENT_ID)
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
